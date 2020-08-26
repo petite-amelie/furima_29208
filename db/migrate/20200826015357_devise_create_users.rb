11d6697e8f14a35a5b5afea20f12aee74578bb58
petite-amelie <pop_ogi0927@icloud.com>
2020-08-26 14:27:58 +0900

    Usersテーブルの作成

--- db/migrate/20200826015357_devise_create_users.rb
+++ db/migrate/20200826015357_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,             null: false
+      t.string :first_name,           null: false
+      t.string :family_name,          null: false
+      t.string :first_name_furigana,  null: false
+      t.string :family_name_furigana, null: false
+      t.date   :birth_day,            null: false
+      t.string :email,                null: false, default: ""
+      t.string :encrypted_password,   null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_08_26_015357) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_furigana", null: false
+    t.string "family_name_furigana", null: false
+    t.date "birth_day", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
