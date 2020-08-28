54581d48898906916c74983f7ae0f82da589b190
petite-amelie <pop_ogi0927@icloud.com>
2020-08-28 12:10:47 +0900

    acyive_hash_prefecture作成と設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :postage_type
+  belongs_to_active_hash :prefecture
 
   validates :name, :price, :introduction, :category_id, :item_condition_id, :prefecture_id,:postage_type_id, :preparation_id, :user_id, presence: true
 
   with_options numericality: { other_than: 1 } do
-    validates :category_id, :item_condition_id, :postage_type_id
+    validates :category_id, :item_condition_id, :postage_type_id,:prefecture_id
   end
 end
