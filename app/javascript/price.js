c6fe7a18167152489a3ee6f349a331f7997a5a44
petite-amelie <pop_ogi0927@icloud.com>
2020-08-28 20:40:50 +0900

    javascript実装

---
+++ app/javascript/price.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function(){
+
+  const price = document.getElementById("item-price")
+  price.addEventListener('input', function(){
+    // 手数料の計算
+    var input_price = price.value
+    var fee = input_price * 0.1
+    // HTMLに手数料の値を渡す
+    const output_fee = document.getElementById("add-tax-price")
+    output_fee.textContent = fee
+    // HTMLに利益の値を渡す
+    var profit = input_price - fee
+    const output_profit = document.getElementById("profit")
+    output_profit.textContent = profit
+  })
+})
