92b1eff9de094a89ed892b772567108de5b2a91c
petite-amelie <pop_ogi0927@icloud.com>
2020-08-28 10:55:47 +0900

    active_hash_item_condition作成と設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
 
   validates :name, :price, :introduction, :category_id, :item_condition_id, :prefecture_id,:postage_type_id, :preparation_id, :user_id, presence: true
 
   with_options numericality: { other_than: 1 } do
-    validates :category_id
+    validates :category_id, :item_condition
   end
 end
