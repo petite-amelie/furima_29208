e38bacf83499f21a94a023e1282d665cd6216811
petite-amelie <pop_ogi0927@icloud.com>
2020-08-28 09:58:46 +0900

    active_hash_category作成と設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :name, :price, :introduction, :category_id, :item_condition_id, :prefecture_id,:postage_type_id, :preparation_id, :user_id, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+  end
 end
