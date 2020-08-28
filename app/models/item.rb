69b1c580a6e2d8964f490f5067a6f904fcc22d06
petite-amelie <pop_ogi0927@icloud.com>
2020-08-28 12:21:45 +0900

    active_hash_preparation作成と設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :postage_type
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation
 
   validates :name, :price, :introduction, :category_id, :item_condition_id, :prefecture_id,:postage_type_id, :preparation_id, :user_id, presence: true
 
   with_options numericality: { other_than: 1 } do
-    validates :category_id, :item_condition_id, :postage_type_id,:prefecture_id
+    validates :category_id, :item_condition_id, :postage_type_id,:prefecture_id,:preparation_id
   end
 end
