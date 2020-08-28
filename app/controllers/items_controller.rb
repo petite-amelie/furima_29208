6eee9a54889c946932f931ea9a20149f7b3cacb4
petite-amelie <pop_ogi0927@icloud.com>
2020-08-28 16:44:47 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,31 @@
 class ItemsController < ApplicationController
   def index
   end
+
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(
+      :item
+    ).permit(
+      :name, :price, :introduction, :category_id, :image,
+      :item_condition_id, :prefecture_id, :postage_type_id, :preparation_id
+    ).merge(
+      user_id: current_user.id
+    )
   end
 end
