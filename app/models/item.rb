6699f6cadce098dd1be8ee523c9981dc0a3e8796
petite-amelie <pop_ogi0927@icloud.com>
2020-08-28 11:51:11 +0900

    active_hash_postage_type作成と設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :item_condition
+  belongs_to_active_hash :postage_type
 
   validates :name, :price, :introduction, :category_id, :item_condition_id, :prefecture_id,:postage_type_id, :preparation_id, :user_id, presence: true
 
   with_options numericality: { other_than: 1 } do
-    validates :category_id, :item_condition
+    validates :category_id, :item_condition_id, :postage_type_id
   end
 end
