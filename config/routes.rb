3815c2e2bc0443466a310674b7a69e9b703bc8ff
petite-amelie <pop_ogi0927@icloud.com>
2020-08-31 19:27:55 +0900

    showアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 root to: "items#index"
-resources :items, only: [:new, :create, :index]
+resources :items, only: [:new, :create, :index] do
+  member do
+    get 'show'
+  end
+end
+
 end
