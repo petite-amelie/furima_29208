f7e64cc3c031ac3c53061a5d4c02e1a5d9420110
petite-amelie <pop_ogi0927@icloud.com>
2020-09-01 17:24:37 +0900

    updateアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :to_top_page, only: [:new]
   before_action :authenticate_user! , except: [:index, :show]
-  before_action :item_parameter, only: [:show, :edit]
+  before_action :item_parameter, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -22,10 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
 
-    if item.update(item_params)
-      redirect_to "/items/#{item.id}"
+    if @item.update(item_params)
+      redirect_to item_path
     else
       render :edit
     end
