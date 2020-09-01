f169068426b7314ce4946836a2772d5e73709e4c
petite-amelie <pop_ogi0927@icloud.com>
2020-09-01 18:00:57 +0900

    destroyアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :to_top_page, only: [:new]
   before_action :authenticate_user! , except: [:index, :show]
-  before_action :item_parameter, only: [:show, :edit, :update]
+  before_action :item_parameter, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -27,6 +27,12 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
