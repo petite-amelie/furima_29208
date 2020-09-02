ea315242a649d985b42278153636fcb01335fb4d
petite-amelie <pop_ogi0927@icloud.com>
2020-09-02 15:03:19 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 root to: "items#index"
 resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+resources :order, only: [:index, :create]
 
 end
