019aa428efeb32ce6990230727e648874674b0e5
petite-amelie <pop_ogi0927@icloud.com>
2020-09-02 16:52:07 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 root to: "items#index"
 resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-resources :order, only: [:index, :create]
+resources :orders, only: [:create] do
+  member do
+    get 'index', to: 'orders#index'
+  end
+end
 
 end
