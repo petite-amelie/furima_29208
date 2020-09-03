b6c48727757cb9fd1daf4a9da1d2dd3d16f8ccac
petite-amelie <pop_ogi0927@icloud.com>
2020-09-04 01:01:36 +0900

    ルーティングのネスト構造

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-root to: "items#index"
-resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-resources :orders, only: [:create] do
-  member do
-    get 'index', to: 'orders#index'
+  root to: "items#index"
+  resources :items do
+    resources :orders, only: [:index, :new, :create]
   end
-end
-
 end
