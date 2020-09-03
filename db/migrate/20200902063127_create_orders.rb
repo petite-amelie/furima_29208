4cbda650742313e1cbca06f11f0448d34c32a796
petite-amelie <pop_ogi0927@icloud.com>
2020-09-04 01:00:49 +0900

    destinationテーブル実装

--- db/migrate/20200902063127_create_orders.rb
+++ db/migrate/20200902063127_create_orders.rb
@@ -4,7 +4,6 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       
       t.integer    :item_id,        null: false, foreign_key: true
       t.integer    :user_id,        null: false, foreign_key: true
-      t.integer    :destination_id, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_063127) do
+ActiveRecord::Schema.define(version: 2020_09_02_093442) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 2020_09_02_063127) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "building_name"
+    t.string "house_number", null: false
+    t.string "phone_number", null: false
+    t.integer "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", null: false
@@ -50,7 +62,6 @@ ActiveRecord::Schema.define(version: 2020_09_02_063127) do
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "item_id", null: false
     t.integer "user_id", null: false
-    t.integer "destination_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
